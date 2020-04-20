7b34c0ba88001171805c33cd11244a422a9d331a
WANGXIAOBO558 <WANGXIAOBO558@pingan.com.cn>
2020-04-21 00:27:58 +0800

    完成Vue实例上的数据代理

--- vue/src/index.js
+++ vue/src/index.js
@@ -5,8 +5,23 @@ class Vue {
     this.$data = options.data || {};
     this.$el = document.getElementById(options.el);
 
+    // 数据代理
+    this._proxyData(this.$data);
+
     // 数据劫持
     new Observer(this.$data);
+  }
+  _proxyData (data) {
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        get () {
+          return data[key];
+        },
+        set (newValue) {
+          data[key] = newValue;
+        }
+      })
+    })
   }
 }
 
