8db2311bcde5306ee0becdc176dfc42154600144
WANGXIAOBO558 <WANGXIAOBO558@pingan.com.cn>
2020-04-21 00:40:53 +0800

    添加遍历节点方法

--- vue/src/compiler.js
+++ vue/src/compiler.js
@@ -1,6 +1,20 @@
 class Compiler {
   constructor (context) {
-    console.log(context);
+    this.$el = context.$el;
+    this.data = context.$data;
+    if (this.$el) {
+      // 将原始模板dom转换为文档片段
+      const $fragment = this.nodeToFragment(this.$el);
+    }
+  }
+  nodeToFragment (node) {
+    const fragment = document.createDocumentFragment();
+    if (node.childNodes && node.childNodes.length > 0) {
+      node.childNodes.forEach(child => {
+        console.log(child);
+      })
+    }
+    return fragment;
   }
 }
 
