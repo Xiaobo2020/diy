4ed98ecc7bbd09abe58915d6365515229a4e65f2
WANGXIAOBO558 <WANGXIAOBO558@pingan.com.cn>
2020-04-21 15:19:09 +0800

    添加过滤获取有效DOM模板节点

--- vue/src/compiler.js
+++ vue/src/compiler.js
@@ -5,17 +5,33 @@ class Compiler {
     if (this.$el) {
       // 将原始模板dom转换为文档片段
       const $fragment = this.nodeToFragment(this.$el);
+
     }
   }
   nodeToFragment (node) {
     const fragment = document.createDocumentFragment();
     if (node.childNodes && node.childNodes.length > 0) {
       node.childNodes.forEach(child => {
-        console.log(child);
+        if (!this.ignorable(child)) {
+          fragment.appendChild(child);
+        }
       })
     }
     return fragment;
   }
+  ignorable (node) {
+    const regText = /[\r\n\t]+/;
+    if (node.nodeType === 8) {
+      // 注释
+      return true;
+    } else if (node.nodeType === 3 && node.textContent.match(regText)) {
+      // 文本节点，但是无效
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
 
 export default Compiler;
